983089fab016f0b6567c874935fd8b0220826a45
Philipp Kief <philipp.kief@gmx.de>
2022-08-15 22:38:37 +0200

    Add wasd support for controls

--- src/app/controls.ts
+++ src/app/controls.ts
@@ -7,22 +7,19 @@ type SwipeEvent = {
 
 type SwipeDirection = 'top' | 'right' | 'bottom' | 'left';
 type KeyboardDirection = 'ArrowUp' | 'ArrowRight' | 'ArrowDown' | 'ArrowLeft';
+type KeyboardDirection2 = 'w' | 'a' | 's' | 'd';
 
 export class Controls {
   constructor(private game: Game) {}
 
   handleKeydown() {
     return (event: KeyboardEvent) => {
-      if (event.key.startsWith('Arrow')) {
-        this.initiallyStartGame();
-      }
       this.handleControlInput(event.key as KeyboardDirection);
     };
   }
 
   handleSwipe() {
     return (event: CustomEvent<SwipeEvent>) => {
-      this.initiallyStartGame();
       this.handleControlInput(event.detail.direction);
     };
   }
@@ -33,21 +30,29 @@ export class Controls {
     }
   }
 
-  private handleControlInput(input: KeyboardDirection | SwipeDirection) {
+  private handleControlInput(
+    input: KeyboardDirection | KeyboardDirection2 | SwipeDirection
+  ) {
+    this.initiallyStartGame();
+
     switch (input) {
       case 'ArrowUp':
+      case 'w':
       case 'top':
         this.game.snake.switchDirection('up');
         break;
       case 'ArrowDown':
+      case 's':
       case 'bottom':
         this.game.snake.switchDirection('down');
         break;
       case 'ArrowLeft':
+      case 'a':
       case 'left':
         this.game.snake.switchDirection('left');
         break;
       case 'ArrowRight':
+      case 'd':
       case 'right':
         this.game.snake.switchDirection('right');
         break;
